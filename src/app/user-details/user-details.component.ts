f607a56969e1d688a933484fca1338f1662df533
Adrion10 <lacehila@gmail.com>
2022-08-19 23:29:46 +0200

    item.name item.email

--- src/app/user-details/user-details.component.ts
+++ src/app/user-details/user-details.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
-  styleUrls: ['./user-details.component.css']
+  styleUrls: ['./user-details.component.css'],
 })
 export class UserDetailsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor() {}
+  @Input() item: { name: string; email: string } = { name: '', email: '' };
+  ngOnInit(): void {}
 }
